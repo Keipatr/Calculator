3c2989f7b8d48b839d039fe2d48f4295818bfee5
Kei <kpatrick@student.ciputra.ac.id>
2023-10-01 03:21:15 +0700

    Start ui

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var calcButton: [UIButton]!
+    
+    @IBOutlet weak var lbResult: UILabel!
+    
+    var first = "";
+    var second = "";
+    var simbol = "";
+    var result = "";
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for button in calcButton{
+            button.layer.cornerRadius = button.frame.size.height/2;
+//            button.titleLabel?.font = UIFont.systemFont(ofSize: 2400.0, weight: .bold)
+        }
     }
-
-
+    
+    @IBAction func btnNumber(_ sender: UIButton) {
+        if simbol == ""{
+            first += String(sender.tag);
+            lbResult.text = first;
+        }
+    }
+    
+    @IBAction func btnAC(_ sender: UIButton) {
+        first = "";
+        second = "";
+        simbol = "";
+        result = "";
+    }
 }
 
